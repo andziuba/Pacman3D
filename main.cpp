7d4b3a277d60f0bb06b2a445e95608d1679fa966
andziuba <121431990+andziuba@users.noreply.github.com>
2024-06-06 22:20:31 +0200

    shader fix

--- main.cpp
+++ main.cpp
@@ -45,11 +45,6 @@ Model* logoModel;
 // Zmienna globalna dla wierzcholkow modelu labiryntu
 std::vector<float> mazeVertices;
 
-// Swiatło
-glm::vec4 lightPos1 = glm::vec4(5.0f, 10.0f, 5.0f, 1.0f);  // Gorne-prawe
-glm::vec4 lightPos2 = glm::vec4(-5.0f, 10.0f, 5.0f, 1.0f); // Gorne-lewe
-glm::vec4 ks = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
-
 // Uchwyty tekstur
 GLuint texWalls;
 GLuint texFloor;
